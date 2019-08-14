2dc9c134fe9fd62d96cd2d98544afff396d800ea
Sam Cate <samcate@Sams-MacBook-Pro-2.local>
2019-08-13 20:31:17 -0400

    initializing website

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('mainApp', ['ui.router'])
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+
+  // // HOME STATES AND NESTED VIEWS ========================================
+  .state('home', {
+      url: '/',
+      templateUrl: '../partials/home.html'
+  })
+});
---
+++ public/js/controllers/mainCtrl.js
@@ -0,0 +1,6 @@
+app.controller('mainCtrl', ['$scope', '$http', '$window', '$compile', function($scope, $http, $window, $compile) {
+  
+  var hi = ' bitch';
+  $scope.test = 'hello' + hi;
+  
+}]);
